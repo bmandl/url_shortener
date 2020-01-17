76110fe1a9a9273ed5f727bf01688c2e9f86539a
Blaž Mandl <blaz.mandl@gmail.com>
2020-01-17 20:07:29 +0100

    Database connection working ok

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var mongoClient = require('mongodb');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+var dns = require("dns");
+var bodyParser = require('body-parser');
+const ShortUrl = require('nanoid/generate');
+
+dotenv.config();
 
 var cors = require('cors');
 
@@ -11,27 +17,66 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+/** this project needs a db !! **/
+mongoose.connect(process.env.MONGOLAB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, 'connection error:'));
+
+var urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: {
+    type: Number,
+    index: true
+  }
+});
+
+var Url = mongoose.model('url', urlSchema);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.post("/api/shorturl/new", (req, res) => {
+
+  var newUrl = new Url(({ original_url: req.body.url , short_url: ShortUrl('1234567890',3)}));
+
+  Url.find({ original_url: newUrl.original_url }, (err, data) => {
+    if (err) return console.err(err);
+    if (!data.length) {
+      newUrl.save((err1, entry) => {
+        if (err1) return console.err(err1);
+      });
+      res.json(newUrl);
+      console.log("Url added");      
+    }
+    else {
+      console.log("Url already exists");
+      res.json(data);
+    }
+  });
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+/*Url.find((err, data) => {
+  if (err) return console.err(err);
+  console.log(data);
+});*/
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log(`Node.js listening on port ${port}...`);
 });
